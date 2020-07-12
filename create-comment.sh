c005c88beda3041e3863487a7522c626787ede2d
Victoria Drake <hello@victoria.dev>
2020-07-12 17:18:06 -0400

    Update allowed characters

--- create-comment.sh
+++ create-comment.sh
@@ -3,5 +3,5 @@
 set -eo pipefail
 
 if [[ "${ISSUE_URL: -2}" == "/1" ]]; then
-    printf '%b\n' "\r\n<div class=\"comment\"><p>${AUTHOR} says:</p><p>${COMMENT//[^a-zA-Z0-9_\s]}</p><p>${DATE}</p></div>\r\n" > ${FOLDER}/${ID}.html
+    printf '%b\n' "\r\n<div class=\"comment\"><p>${AUTHOR} says:</p><p>${COMMENT//[^a-zA-Z0-9_\\s]}</p><p>${DATE}</p></div>\r\n" > ${FOLDER}/${ID}.html
 fi
