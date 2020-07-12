c72d1eb3ad583ed9ee1425eadb0e3b2b52e17235
Victoria Drake <hello@victoria.dev>
2020-07-12 16:24:41 -0400

    Move variables to an env block

---
+++ create-comment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [[ "${ISSUE_URL: -2}" == "/1" ]]; then
+    printf '%b\n' "\r\n<div class=\"comment\"><p>${AUTHOR} says:</p><p>${COMMENT//[^a-zA-Z0-9_]}</p><p>${DATE}</p></div>\r\n" > ${FOLDER}/${ID}.html
+fi
